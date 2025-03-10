265a9d9e93b3dceb6478504ec820834a5b1b69c2
chima1968 <chimarokeumunna98@gmail.com>
2025-03-10 22:35:57 +0100

    mods

--- models/users.js
+++ models/users.js
@@ -7,7 +7,7 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "procurement_officer","normal_user"], default: "procurement_officer" },
+  role: { type: String, enum: ["admin", "procurement_officer","normal_user"], default: "staff" },
 }, { timestamps: true });
 
 UserSchema.plugin(timestamp);
