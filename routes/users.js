c36d96c07a6aed0a09bc385d8e5072ff7756085c
chima1968 <chimarokeumunna98@gmail.com>
2025-03-19 13:13:17 +0100

    update

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const {Router}=require('express')
-const User=require('../models/users_')
+const User=require('../models/users_');
+const users_ = require('../models/users_');
 
 const router=Router()
 
@@ -9,6 +10,27 @@ router.get("/", async (req, res) => {
     res.json(user_data);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
+  }
+});
+router.get("/:email", async (req, res) => {
+  try {
+    const { email } = req.params;
+    
+    if (!email) {
+      return res.status(400).json({ error: "Email is required" });
+    }
+
+    // Fetch user orders
+    const userRequests = await users_.find({ email });
+
+    if (!userRequests.length) {
+      return res.status(404).json({ message: "No user found for this email" });
+    }
+
+    res.status(200).json(userRequests);
+  } catch (error) {
+    console.error("Error fetching user :", error);
+    res.status(500).json({ error: "Failed to retrieve user" });
   }
 });
 //create the user
@@ -26,6 +48,23 @@ router.post('/',async (req,res)=>{
       res.status(400).json({ message: "Error User not created" });
       console.error(error)
     }
+})
+router.put('/:email',async(req,res)=>{
+  try{
+    const {password,email}=req.body
+
+
+    const updatedpassword=await users_.findOneAndUpdate(
+      {email,password},
+      {new:true}
+
+    )
+    if (!updatedpassword) {
+      return res.status(404).json({ message: "User not found" });
+    }
+  }catch{
+    res.status(400).json({ message: "Error updating User", error });
+  }
 })
 
 //delete User
