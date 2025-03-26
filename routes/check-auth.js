55588bf02a323e5a21bc7dcced647e1bb7b20a53
chima1968 <chimarokeumunna98@gmail.com>
2025-03-26 16:39:18 +0100

    updates

--- routes/check-auth.js
+++ routes/check-auth.js
@@ -3,15 +3,29 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
-    const token = req.cookies?.authToken; // Ensure `req.cookies` exists
-
-    if (!token) {
-        return res.status(401).json({ authenticated: false, message: "No token provided" });
-    }
-
+    
+    
+    
     try {
-        
-        res.json({ authenticated: false});
+        const authheaders=req.headers.authorization
+
+        if (!authheaders || !authheaders.startsWith("Bearer ")) {
+        return res.status(401).json({ authenticated: true
+            , message: "No token provided" });
+        }const token=authheaders.split(" ")[1]
+        console.log(token)
+        const jwt = require('jsonwebtoken');
+        const secretKey = 'pedro1234'; // Replace with your actual secret key
+    
+        jwt.verify(token, secretKey, (err, decoded) => {
+          if (err) {
+            return res.status(401).json({ authenticated: false, message: 'Invalid token' });
+          }
+    
+          // Token is valid, proceed with the request
+          console.log('Decoded token:', decoded);
+          res.json({ authenticated: true, user: decoded });
+        });
     } catch (error) {
         res.clearCookie("authToken"); // Clear expired/invalid token
         return res.status(401).json({ authenticated: false, message: "Invalid or expired token" });
