c027b2704f9d60cebf38afd4c0e9a0e28f14ed90
chima1968 <chimarokeumunna98@gmail.com>
2025-05-13 09:52:12 +0100

    updated structure

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const circuitBreaker = require("opossum");
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: './.env' });
 
 
 
--- models/Department.js
+++ models/Department.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User=require("../models/users_")
+const User=require("./users_")
 const departmentSchema = new mongoose.Schema({
   name: {
     type: String,
