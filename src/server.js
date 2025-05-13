5c92c69687cbfbc3f5cace04e69ada15eca4c763
chima1968 <chimarokeumunna98@gmail.com>
2025-05-13 09:41:34 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
-// Load environment variables
-require("dotenv").config();
 
 // Core modules
 const path = require("path");
-require('dotenv').config();
+
 // Third-party packages
 const express = require("express");
 const cors = require("cors");
